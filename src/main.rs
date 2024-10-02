97856ca08888ed84424debf4363bd1a70dd1ed9e
David Wallace Croft <david@CroftSoft.com>
2024-10-01 19:37:38 -0500

    plugins

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,21 @@ struct Name(String);
 #[derive(Component)]
 struct Person;
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+  fn build(
+    &self,
+    app: &mut App,
+  ) {
+    app.add_systems(Startup, add_people);
+    app.add_systems(
+      Update,
+      (hello_world, (update_people, greet_people).chain()),
+    );
+  }
+}
+
 fn add_people(mut commands: Commands) {
   commands.spawn((Person, Name("Alpha Bravo".to_string())));
   commands.spawn((Person, Name("Charlie Delta".to_string())));
@@ -24,8 +39,8 @@ fn hello_world() {
 
 fn main() {
   App::new()
-    .add_systems(Startup, add_people)
-    .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+    .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
     .run();
 }
 
@@ -33,7 +48,7 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
   for mut name in &mut query {
     if name.0 == "Charlie Delta" {
       name.0 = "Golf Hotel".to_string();
-      
+
       break;
     }
   }
