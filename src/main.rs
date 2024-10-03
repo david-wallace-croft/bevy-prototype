50b4cb9cae62a7a26a8f014010a1a84e87e2b37f
David Wallace Croft <david@CroftSoft.com>
2024-10-02 20:02:21 -0500

    timer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 use bevy::prelude::*;
 
+fn main() {
+  App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
+    .run();
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+struct HelloPlugin;
+
 #[derive(Component)]
 struct Name(String);
 
 #[derive(Component)]
 struct Person;
 
-pub struct HelloPlugin;
-
 impl Plugin for HelloPlugin {
   fn build(
     &self,
     app: &mut App,
   ) {
+    app.insert_resource(GreetTimer(Timer::from_seconds(
+      2.,
+      TimerMode::Repeating,
+    )));
+
     app.add_systems(Startup, add_people);
-    app.add_systems(
-      Update,
-      (hello_world, (update_people, greet_people).chain()),
-    );
+
+    app.add_systems(Update, (update_people, greet_people).chain());
   }
 }
 
@@ -27,21 +40,16 @@ fn add_people(mut commands: Commands) {
   commands.spawn((Person, Name("Echo Foxtrot".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-  for name in &query {
-    println!("hello {}!", name.0);
+fn greet_people(
+  query: Query<&Name, With<Person>>,
+  time: Res<Time>,
+  mut timer: ResMut<GreetTimer>,
+) {
+  if timer.0.tick(time.delta()).just_finished() {
+    for name in &query {
+      println!("hello {}!", name.0);
+    }
   }
-}
-
-fn hello_world() {
-  println!("Hello, World!");
-}
-
-fn main() {
-  App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugins(HelloPlugin)
-    .run();
 }
 
 fn update_people(mut query: Query<&mut Name, With<Person>>) {
